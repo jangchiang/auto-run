76f7f98747521cde840776fafcfad39532b629b7
Unknown-user-C <42628079+jangchiang@users.noreply.github.com>
2022-10-28 21:29:04 +0700

    Add files via upload

---
+++ auto run.py
@@ -0,0 +1,34 @@
+'''
+import the os library and use the system to sript the auto run python file
+'''
+import os
+os.system('python /xxxx/x.py')  # change the directory and file name
+os.system('python /xxxx/x.py')  # change the directory and file name
+os.system('python /xxxx/x.py')  # change the directory and file name
+os.system('python /xxxx/x.py')  # change the directory and file name
+
+'''if want to run more source just copy and paste the script'''
+
+
+'''also you it can run in the alternative method follow on'''
+# for set the direction
+'''
+def fa()
+    # TODO: file A
+
+def fb()
+    # TODO: file B
+
+def fc()
+    # TODO: file C
+'''
+# for set run the command
+'''
+    import file_a
+    import file_b
+    import file_c
+
+    run_a=a.fa()
+    run_b=b.fb()
+    run_c=c.fc()
+'''
